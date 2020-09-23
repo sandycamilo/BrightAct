6b4575f88ee728404c7bcc717450024fd46b5f9a
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-09-23 14:51:16 -0700

    mapped Flask routes to Python functions

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello, world'
+def landing_page():
+    return render_template('landing_page.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about_page.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=os.eviron.get('PORT', 5000))
