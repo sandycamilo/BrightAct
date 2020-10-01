0360728972d1b5b8f3b8c66d9d84650c4f0d832d
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-10-01 15:07:36 -0700

    app is able to read data from a spreadsheet

--- app.py
+++ app.py
@@ -13,7 +13,17 @@ scopes = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scopes)
                                                               
+client = gspread.authorize(credentials)
+gsheet = client.open("BrightAct Get Involved Form").sheet1
 
+app = Flask(__name__)
+mail = Mail()
+# config
+# app.run('host'=localhost, debug=True)
+# app.config.from_pyfile('config.py')
+
+lst_of_hashes = gsheet.get_all_records()
+print(lst_of_hashes)
 
 app.secret_key = os.getenv('SECRET_KEY')
 
